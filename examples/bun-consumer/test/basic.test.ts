9233754943abf120e1a5f9076bf2aeb6aaa28448
Sujeet Kc <sujeetkc45@gmail.com>
2025-12-01 19:43:11 -0400

    test: add mock function tests for Vite consumer

--- examples/bun-consumer/test/basic.test.ts
+++ examples/bun-consumer/test/basic.test.ts
@@ -1,10 +1,24 @@
-import { describe, it, expect } from "bun:test";
+import { describe, it, expect, mock } from "bun:test";
 import { def, get } from "bdd-lazy-var-next/bun";
 
 def("foo", () => "bar");
+def("func", () => mock(() => "baz"));
 
 describe("Vite Consumer Basic", () => {
   it("returns correct value", () => {
     expect(get("foo")).toBe("bar");
+  });
+
+  it("mocks function correctly", () => {
+    const res = get("func")();
+    expect(res).toBe("baz");
+    expect(get("func")).toHaveBeenCalled();
+    expect(get("func")).toHaveBeenCalledTimes(1);
+  });
+
+  it("allows multiple calls to mocked function", () => {
+    get("func")();
+    get("func")();
+    expect(get("func")).toHaveBeenCalledTimes(2);
   });
 });
